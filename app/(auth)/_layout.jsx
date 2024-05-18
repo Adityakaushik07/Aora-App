a244bb160aeaa18ea429cc98092b97c3ce460e05
Aditya-1322 <kaushikaditya985@gmail.com>
2024-05-18 11:37:48 +0530

    add bottom tabs

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LayoutAuth = () => {
+  return (
+    <View>
+      <Text>LayoutAuth</Text>
+    </View>
+  )
+}
+
+export default LayoutAuth
+
+const styles = StyleSheet.create({})
