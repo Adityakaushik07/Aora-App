c61492cee72d8766dab522162df8068378efb156
Aditya-1322 <kaushikaditya985@gmail.com>
2024-05-24 06:52:28 +0530

    add logic to sign in and sign up

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, Text, View } from "react-native";
-import {Redirect,router} from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
+import Loader from "../components/Loader";
+import { useGlobalContext } from "../context/GlobalProvider";
 
-export default function App() {
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+      <Loader isLoading={loading} />
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -18,13 +30,14 @@ export default function App() {
 
           <Image
             source={images.cards}
-            className="max-w-[380px] h-[300px]"
+            className="max-w-[380px] w-full h-[298px]"
             resizeMode="contain"
           />
 
           <View className="relative mt-5">
             <Text className="text-3xl text-white font-bold text-center">
-              Discover Endless Possibilities with{" "}
+              Discover Endless{"\n"}
+              Possibilities with{" "}
               <Text className="text-secondary-200">Aora</Text>
             </Text>
 
@@ -36,21 +49,20 @@ export default function App() {
           </View>
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-            Where Creativity meets innovation: embrake on journey of limitless
-            exploration with Aora
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
           </Text>
 
-        <CustomButton
+          <CustomButton
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
-         
-
         </View>
       </ScrollView>
 
-      <StatusBar backgroundColor="#161622" style="light"></StatusBar>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
-}
+};
+export default Welcome;
